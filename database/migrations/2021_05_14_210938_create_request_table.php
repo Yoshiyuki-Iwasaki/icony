7da6d22d40994003af72ad064a5cd4d8805258c6
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-05-15 06:36:53 +0900

    [add] db作成

---
+++ database/migrations/2021_05_14_210938_create_request_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('requested_user_id')->index();
+            $table->unsignedBigInteger('requesting_user_id')->index();
+            $table->foreign('requested_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('requesting_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('content')->nullable();
+            $table->string('image_id')->references('id')->on('request_image')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request');
+    }
+}
---
+++ database/migrations/2021_05_14_212512_create_request_image_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_image', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('url');
+            $table->string('format');
+            $table->string('size');
+            $table->string('width');
+            $table->string('height');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_image');
+    }
+}

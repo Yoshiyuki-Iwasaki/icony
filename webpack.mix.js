0f2055d80cf266cc6ab62f860b092d262a44721e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-10 22:50:15 +0900

    add typescript.

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,7 @@ const mix = require("laravel-mix");
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.js("resources/js/app.js", "public/js")
+mix.ts("resources/ts/App.tsx", "public/js")
     .react()
     .sass("resources/sass/app.scss", "public/css", [
         //

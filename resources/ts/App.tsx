5271789ba21f8a678c9b7a29270935ab35c58e34
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-14 09:30:02 +0900

    create login function.

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -8,6 +8,7 @@ import SignIn from "./pages/SignIn";
 import OrderDetail from "./components/OrderDetail";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
+import Auth from "./components/Auth";
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
                 <Route exact path="/" component={Top} />
                 <Route exact path="/signup" component={SignUp} />
                 <Route exact path="/signin" component={SignIn} />
+                <Route exact path="/auth" component={Auth} />
                 <Route path="/orders/:id" component={OrderDetail} />
             </BrowserRouter>
         </Layout>
--- resources/ts/components/Header.tsx
+++ resources/ts/components/Header.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link, Router } from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
+    const [user, setUser] = useState<any>(null);
+    // ブラウザリロード時にログイン済みか判定
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    // 認証ユーザを取得
+    const getUser = () => {
+        axios
+            .get("/api/user")
+            .then((res) => {
+                console.log("[getUser]ログイン済み");
+                console.log("res01", res);
+                setUser(res.data);
+            })
+            .catch((err) => {
+                console.log("[getUser]ログインしてません");
+            });
+    };
     return (
         <HeaderLayout>
             <Inner>
@@ -10,6 +30,7 @@ const Header = () => {
                     <Link to="/">Icony</Link>
                 </Title>
                 <LeftArea>
+                    <Auth to="/auth">Auth</Auth>
                     <SignUp to="/signup">Sign Up</SignUp>
                     <SignIn to="/signin">Sign In</SignIn>
                 </LeftArea>
@@ -34,5 +55,6 @@ const Title = styled.h1`
     font-weight: 700;
 `;
 const LeftArea = styled.div``;
+const Auth = styled(Link)``;
 const SignUp = styled(Link)``;
 const SignIn = styled(Link)``;

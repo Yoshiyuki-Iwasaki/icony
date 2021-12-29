c874f51351a6c4a1839ac8a3de3d56dd8d7dc434
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-30 04:24:25 +0900

    create hooks directory.

--- resources/ts/components/Header.tsx
+++ resources/ts/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { HeaderType } from '../type/header';
-import useDarkMode from "./useDarkMode";
+import useDarkMode from "../hook/useDarkMode";
 
 const Header: React.FC<HeaderType> = ({ getUser }) => {
     const [theme, setTheme] = useDarkMode();
